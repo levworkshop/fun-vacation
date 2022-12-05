0276cb52aab11aa6c07c19bc917a6c75b3c793c4
levworkshop <noreply@githubusers.com>
2022-12-05 20:55:07 +0200

    get one in server

--- server/controllers/vacations.js
+++ server/controllers/vacations.js
@@ -13,6 +13,29 @@ module.exports = {
         }
     },
 
+    getItem: async function (req, res, next) {
+        try {
+            const scheme = joi.object({
+                _id: joi.string().required(),
+            });
+
+            const { error, value } = scheme.validate({ _id: req.params.id });
+
+            if (error) {
+                console.log(error.details[0].message);
+                res.status(400).send('invalid data');
+                return;
+            }
+
+            const result = await Vacation.findOne({ _id: value._id });
+            res.json(result);
+        }
+        catch (err) {
+            console.log(err);
+            res.status(400).send('error getting vacations');
+        }
+    },
+
     add: async function (req, res, next) {
         try {
             const scheme = joi.object({
