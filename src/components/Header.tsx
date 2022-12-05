3a3af069bb625d7607aecf29db37bff37f5a32ec
levworkshop <noreply@githubusers.com>
2022-12-05 18:54:22 +0200

    header html+css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 function Header() {
     return (
         <header>
-            <nav className="navbar navbar-dark bg-dark">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container-fluid">
                     <NavLink
                         className="navbar-brand"
@@ -11,23 +11,35 @@ function Header() {
                         <i className="bi-airplane-engines-fill me-2" />
                         Fun Vacation
                     </NavLink>
-                    <NavLink
-                        to="/vacations"
-                    >
-                        Vacations
-                    </NavLink>
 
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <NavLink
-                                className="nav-link"
-                                aria-current="page"
-                                to="/order"
-                            >
-                                Order Now
-                            </NavLink>
-                        </li>
-                    </ul>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <NavLink
+                                    className="nav-link"
+                                    aria-current="page"
+                                    to="/vacations"
+                                >
+                                    Vacations
+                                </NavLink>
+                            </li>
+                        </ul>
+
+                        <ul className="navbar-nav d-flex">
+                            <li className="nav-item">
+                                <NavLink
+                                    className="nav-link"
+                                    aria-current="page"
+                                    to="/order"
+                                >
+                                    Order Now
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
         </header>
