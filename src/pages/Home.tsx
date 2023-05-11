31ae936bc8b3978c140286dc221dc70c7e2fbe98
levworkshop <noreply@githubusers.com>
2023-05-11 20:52:16 +0300

    home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,44 @@
+import Title from "../components/Title";
+
 function Home() {
     return (
-        <>Home</>
+        <>
+            <Title />
+
+            <div className="d-flex">
+                <div>
+                    <input
+                        type="text"
+                        placeholder="Search"
+                        className="form-control me-4"
+                    />
+                </div>
+                <div>
+                    <select
+                        className="form-select"
+                    >
+                        <option>--</option>
+                    </select>
+                </div>
+            </div>
+
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Location</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>01/01/23</td>
+                        <td>Location 1</td>
+                        <td>$50</td>
+                    </tr>
+                </tbody>
+            </table>
+        </>
     );
 }
 
